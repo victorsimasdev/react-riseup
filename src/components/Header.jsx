a1a6203a66af574d2f2420de37e0755f89fadde5
Julio Bezerra <bjulio52@gmail.com>
2025-04-26 16:30:01 -0300

    front-end-enhancement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,18 @@ import Logo from '../assets/logo-no-background.svg'
 export function Header(){
   return (
       <header className="w-full h-[80px] bg-[var(--background-header)] flex items-center text-white justify-between px-9">
-        <img className='h-[45px] w-auto' src={Logo} alt="Logo do site" />
-        <div className='items-center inline-flex gap-[30px]'>
-          <button className='font-black hover:text-[#62CAFF]'>BOTAO</button> 
-          <button className='font-black hover:text-[#62CAFF]'>BOTAO</button> 
-          <button className='font-black hover:text-[#62CAFF]'>BOTAO</button> 
-          <button className='font-black hover:text-[#62CAFF]'>BOTAO</button> 
+        <img className='h-[45px] w-auto cursor-pointerh' src={Logo} alt="Logo do site" />
+        <div className='items-center inline-flex gap-[40px]'>
+          <button className='font-black hover:text-[#62CAFF] transition-colors duration-300 cursor-pointer'>INICIO</button> 
+          <button className='font-black hover:text-[#62CAFF] transition-colors duration-300 cursor-pointer'>CHAT</button> 
+          <button className='font-black hover:text-[#62CAFF] transition-colors duration-300 cursor-pointer'>MAPAS</button> 
+          <button className='font-black hover:text-[#62CAFF] transition-colors duration-300 cursor-pointer'>SOBRE</button> 
         </div>
-        <button className="flex w-24 h-10 px-15 py-2.5 justify-center items-center gap-2.5 rounded-[15px] bg-[var(--button)] font-bold text-[1.1rem]">
+        <a href="#">
+          <button className="flex w-24 h-10 px-15 py-2.5 justify-center items-center gap-2.5 rounded-[15px] bg-[var(--button)] font-bold text-[1.1rem] hover:bg-[#007AB9] border-[3px] border-[#00A6FB] hover:border-[var(--button)] transition-colors duration-300 cursor-pointer">
           Entrar
         </button>
-
+        </a>
       </header>
   );
 }
