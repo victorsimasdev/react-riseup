baa74453736eb60d1e7c4131be9706a89934b323
Julio Bezerra <bjulio52@gmail.com>
2025-04-29 15:03:37 -0300

    feat: Adiciona pagina de primeiros socorros

--- src/pages/PrimeirosSocorrosPage.jsx
+++ src/pages/PrimeirosSocorrosPage.jsx
@@ -3,23 +3,28 @@ import { Header } from '../components/Header';
 
 function PrimeirosSocorrosPage() {
   return (
-    <div>
+    <div className='min-h-screen flex flex-col'>
       <Header/>
-      <div className='pt-20 flex justify-center gap-10'>
+      <h1 className='mt-[100px] text-[color:var(--text)] font-roboto text-[2.5rem] not-italic font-bold leading-none flex justify-center'>Guia de Primeiros Socorros</h1>
+      <div className='flex flex-1 justify-center items-end gap-10'>
         <CardPrimeirosSocorros
+          cardColor={'#CFDBC8'}
           src={"../src/assets/images/primeiros-socorros/queimadura.svg"}
           title={"Queimadura por agua viva"}
           description={"Não esfregue a pele.\nLave com agua do mar (nunca doce).\nAplique vinagre ou solução de bicarbonato."}
         />
         <CardPrimeirosSocorros
-          src={"../src/assets/images/primeiros-socorros/queimadura.svg"}
-          title={"Queimadura por agua viva"}
-          description={"Não esfregue a pele.\nLave com agua do mar (nunca doce).\nAplique vinagre ou solução de bicarbonato."}
+          cardColor={'#6FA097'}
+          src={"../src/assets/images/primeiros-socorros/afogamento.svg"}
+          title={"Afogamento"}
+          description={"Ligue para os serviços de emergência.\nSe souber nadar e for seguro, tente alcançar a vitima com um objeto (corda, prancha)."}
+          reverse
         />
         <CardPrimeirosSocorros
-          src={"../src/assets/images/primeiros-socorros/queimadura.svg"}
-          title={"Queimadura por agua viva"}
-          description={"Não esfregue a pele.\nLave com agua do mar (nunca doce).\nAplique vinagre ou solução de bicarbonato."}
+          cardColor={'#FEEFD2'}
+          src={"../src/assets/images/primeiros-socorros/insolacao.svg"}
+          title={"Insolação"}
+          description={"Leve a vitima para a sombra.\nOfereça água e faça compressas frias.\nNão estoure bolhas e procure um posto de saúde se os sintomas forem graves."}
         />
       </div>
     </div>
