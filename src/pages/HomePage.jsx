be572362bc96f68df03831d715f68d775d26401e
Victor Almeida <victorsimasdev@gmail.com>
2025-04-29 17:51:50 -0300

    fix: Corrigido caminho da logo no menu.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { Header } from '../components/Header';
 import HomePageImage from '../assets/images/homePageImage.png';
+import LogoProjeto from '../assets/images/logo.svg';
 
 function HomePage() {
   return (
@@ -8,7 +9,7 @@ function HomePage() {
       <div className="w-[43rem] h-[23.0625rem] pt-[10rem]" >
         <div className='flex'>
           <h1 className='w-[18.5rem] h-[12.75rem] text-[#001F3F] font-roboto text-[8.1875rem] not-italic font-bold leading-[6.25rem]'>Mar Alert</h1>
-          <img className='pl-[2rem]' src="src/assets/images/logo.svg" alt="Logo do site" />
+          <img className='pl-[2rem]' src={ LogoProjeto } alt="Logo do site" />
         </div>
           <p className='text-[color:var(--text)] font-roboto text-[3.625rem] not-italic font-bold leading-[3.8125rem] pt-[2rem]'>Mais do que um alerta.<br/><span className='text-[color:var(--alert)]'>Um aliado no litoral.</span></p>
       </div>
