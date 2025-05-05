4da85c8a920d4ab0a4c6fcaae248e827cd800e62
Victor Almeida <victorsimasdev@gmail.com>
2025-05-05 16:14:44 -0300

    feat: Add Arquivos.

---
+++ ReactFalandoComBack/Back-End.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors()); // Enable CORS for all routes
+
+app.get('/teste', (req, res) => {
+  console.log("--------ok--------");
+  res.send('Ok');
+});
+
+app.listen(3000);
+console.log('Server running at http://127.0.0.1:3000/');
