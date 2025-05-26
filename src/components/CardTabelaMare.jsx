8e5d87802b38a30fabba238cb375511633e71175
Victor Simas <victorsimasdev@gmail.com>
2025-05-26 15:27:47 -0300

    feat: Adicionado dados de maré via json.

--- src/components/CardTabelaMare.jsx
+++ src/components/CardTabelaMare.jsx
@@ -1,34 +1,33 @@
-export function CardTabelaMare() {
+export function CardTabelaMare({ dia, semana, mares }) {
+  const primeiraSessao = mares.slice(0, 2);
+  const segundaSessao = mares.slice(2, 4);
+
   return (
     <div className="border-4 h-[31.0625rem] w-[15.875rem] rounded-[15px] border-[var(--background-header)]">
       <div className="border-b-4 border-[var(--background-header)] h-[8.13rem] flex flex-col items-center justify-center">
-        <h1>Dia da semana</h1>
-        <h2>Dia do mes</h2>
+        <h1 className="font-bold text-lg">{semana}</h1>
+        <h2 className="text-xl">{dia}</h2>
       </div>
       <div className="border-b-4 border-[var(--background-header)] h-[11.12rem] flex flex-col items-center justify-center">
-        <h1>Mare alta</h1>
+        <h1 className="mb-2 font-semibold">Maré</h1>
         <div className="w-[15.875rem]">
-          <div className="flex justify-evenly">
-            <p>Hora</p> {/*Buscar dados na API*/}
-            <p>Profund</p> {/*Buscar dados na API*/}
-          </div>
-          <div className="flex justify-evenly">
-            <p>Hora</p> {/*Buscar dados na API*/}
-            <p>Profund</p> {/*Buscar dados na API*/}
-          </div>
+          {primeiraSessao.map((m, i) => (
+            <div key={i} className="flex justify-evenly">
+              <p>{m.hora}</p>
+              <p>{m.altura.toFixed(2)} m</p>
+            </div>
+          ))}
         </div>
       </div>
       <div className="h-[11.81rem] flex flex-col items-center justify-center">
-        <h1>Mare alta</h1>
+        <h1 className="mb-2 font-semibold">Maré</h1>
         <div className="w-[15.875rem]">
-          <div className="flex justify-evenly">
-            <p>Hora</p> {/*Buscar dados na API*/}
-            <p>Profund</p> {/*Buscar dados na API*/}
-          </div>
-          <div className="flex justify-evenly">
-            <p>Hora</p> {/*Buscar dados na API*/}
-            <p>Profund</p> {/*Buscar dados na API*/}
-          </div>
+          {segundaSessao.map((m, i) => (
+            <div key={i} className="flex justify-evenly">
+              <p>{m.hora}</p>
+              <p>{m.altura.toFixed(2)} m</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
