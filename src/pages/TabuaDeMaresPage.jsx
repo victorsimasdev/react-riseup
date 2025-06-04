3044a907e1d4ecf2bdf701a3825ae66715def38e
Victor <victorsimasazevedo@gmail.com>
2025-06-04 10:55:57 -0300

    feat: quick update

--- src/pages/TabuaDeMaresPage.jsx
+++ src/pages/TabuaDeMaresPage.jsx
@@ -9,36 +9,36 @@ const dadosMares = [
   //      { hora: "14:38", altura: 2.41 },
   //      { hora: "20:53", altura: 0.24 }
   //  ]},
-  { dia: 04, semana: "QUA", mares: [
+  { dia: 4, semana: "QUA", mares: [
       { hora: "02:55", altura: 2.37 },
       { hora: "09:12", altura: 0.11 },
       { hora: "15:23", altura: 2.44 },
       { hora: "21:36", altura: 0.24 }
   ]},
-  { dia: 05, semana: "QUI", mares: [
+  { dia: 5, semana: "QUI", mares: [
       { hora: "03:44", altura: 2.43 },
       { hora: "10:01", altura: 0.07 },
       { hora: "16:14", altura: 2.40 },
       { hora: "22:25", altura: 0.28 }
   ]},
-  { dia: 06, semana: "SEX", mares: [
+  { dia: 6, semana: "SEX", mares: [
       { hora: "04:27", altura: 2.42 },
       { hora: "10:51", altura: 0.11 },
       { hora: "17:02", altura: 2.31 },
       { hora: "23:08", altura: 0.35 }
   ]},
-  { dia: 07, semana: "SÁB", mares: [
+  { dia: 7, semana: "SÁB", mares: [
       { hora: "05:17", altura: 2.37 },
       { hora: "11:46", altura: 0.20 },
       { hora: "17:55", altura: 2.18 }
   ]},
-  { dia: 08, semana: "DOM", mares: [
+  { dia: 8, semana: "DOM", mares: [
       { hora: "00:01", altura: 0.47 },
       { hora: "06:08", altura: 2.27 },
       { hora: "12:36", altura: 0.33 },
       { hora: "18:49", altura: 2.03 }
   ]},
-  { dia: 09, semana: "SEG", mares: [
+  { dia: 9, semana: "SEG", mares: [
       { hora: "00:55", altura: 0.59 },
       { hora: "07:02", altura: 2.15 },
       { hora: "13:40", altura: 0.47 },
